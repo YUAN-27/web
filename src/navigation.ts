139f075144bf632dfa0f26b5600c982e2e419cdf
YUAN-27 <1249850641@qq.com>
2025-12-17 21:14:56 +0800

    页面升级

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,119 +3,66 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: '首页',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: '你的爱情',
+          href: getPermalink('/404.astro'),
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
+          text: '你的余额',
+          href: getPermalink('/404.astro'),
         },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
+
       ],
     },
+   
     {
-      text: 'Pages',
+      text: '博客',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
+          text: '内容',
           href: getBlogPermalink(),
         },
+       
+      ],
+    },
         {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+      text: '关于我',
+      links: [
+        {
+          text: '身世',
+          href: '#',
         },
         {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
+          text: '教育经历',
+          href: '#',
         },
         {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
+          text: '未来发展',
+          href: '404.astro',
         },
       ],
     },
     {
-      text: 'Widgets',
-      href: '#',
+      text: '联系我',
+      links: [
+        {
+          text: 'QQ',
+          href: 'https://user.qzone.qq.com/2817108368',
+        },
+        {
+          text: 'Bilibili',
+          href: 'https://space.bilibili.com/208259?spm_id_from=333.337.0.0',
+        },
+        {
+          text: 'Tel 18627927233',
+        }
+       
+      ],
     },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/YUAN-27/web', target: '_blank' }],
+  actions: [{ text: 'GitHub', href: 'https://github.com/YUAN-27/web', target: '_blank' }],
 };
 
 export const footerData = {
@@ -132,51 +79,20 @@ export const footerData = {
         { text: 'Resources', href: '#' },
       ],
     },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
+
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: '条款', href: getPermalink('/terms') },
+    { text: '隐私政策', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'bilibili', icon: 'tabler:brand-bilibili', href: 'https://space.bilibili.com/208259?spm_id_from=333.337.0.0' },
+    { ariaLabel: 'QQ', icon: 'tabler:brand-qq', href: 'https://user.qzone.qq.com/2817108368' },
+    { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1' },
+    { ariaLabel: 'Wechat', icon: 'tabler:brand-weixin',type:'wechat', qrcode: getAsset('/images/wechat-qrcode.png') , href: '#' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/YUAN-27/web' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/YUAN-27"> YUAN</a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/YUAN-27"> YUAN</a> · 版权所有.
   `,
 };
